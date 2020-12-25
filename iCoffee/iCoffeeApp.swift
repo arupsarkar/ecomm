4f965691f98b8cfd11f073158026b42b856082ec
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2020-12-24 21:54:21 -0500

    Initial Commit

---
+++ iCoffee/iCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoffeeApp.swift
+//  iCoffee
+//
+//  Created by Arup Sarkar on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct iCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
